358384ac398a5c30176ee9b5599ba614d02a509a
dpplweb <dppl.web@gmail.com>
2012-02-03 12:22:49 -0600

    made more changes to dppl heart theme

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -51,10 +51,18 @@ function readCookie(name) {
   return null;
 }
 
+//temporary image hack code
+function changeImage(){
+  var img = document.getElementsByTagName("img")[1];
+  img.style.position = "absolute";
+  img.style.top = "36";
+}
+
 window.onload = function(e) {
   var cookie = readCookie("style");
   var title = cookie ? cookie : getPreferredStyleSheet();
   setActiveStyleSheet(title);
+  changeImage();
 }
 
 window.onunload = function(e) {
