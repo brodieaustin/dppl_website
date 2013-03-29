ef49e121e7e5b3123524a59d1d87b60cd6a4fc08
dpplweb <dppl.web@gmail.com>
2013-03-29 17:12:53 -0500

    fixes to bc list plugin for blogs

--- tools/bc/list-view/js/list-view.js
+++ tools/bc/list-view/js/list-view.js
@@ -1,38 +1,42 @@
 if (typeof jQuery === "undefined") {
   loadjQuery("//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js", verifyJQueryCdnLoaded);
-} else 
+} else {
   main();
+}
 
 function verifyJQueryCdnLoaded() {
-  if (typeof jQuery === "undefined")
+  if (typeof jQuery === "undefined"){
     loadjQuery("script/jquery-1.8.2.js", main);
-  else
+  }
+  else{
     main();
+  }
 }
 
 function loadjQuery(url, callback) {
-  var script_tag = document.createElement('script');
-  script_tag.setAttribute("src", url)
-  script_tag.onload = callback; // Run callback once jQuery has loaded
-  script_tag.onreadystatechange = function () { // Same thing but for IE
-    if (this.readyState == 'complete' || this.readyState == 'loaded') callback();
+  if (document.getElementById('bc-list-jquery') === null){
+	  var script_tag = document.createElement('script');
+	  script_tag.setAttribute('id', 'bc-list-jquery');
+	  script_tag.setAttribute("src", url)
+	  script_tag.onload = callback; // Run callback once jQuery has loaded
+	  script_tag.onreadystatechange = function () { // Same thing but for IE
+		if (this.readyState == 'complete' || this.readyState == 'loaded') callback();
+	  }
+	  script_tag.onerror = function() {
+		loadjQuery("script/jquery-1.8.2.js", main);
+	  }
+	  document.getElementsByTagName("head")[0].appendChild(script_tag);
   }
-  script_tag.onerror = function() {
-    loadjQuery("script/jquery-1.8.2.js", main);
-  }
-  document.getElementsByTagName("head")[0].appendChild(script_tag);
 }
 
 function main() {
-  if (typeof jQuery === "undefined")
-    alert("jQuery not loaded.");
-
   $(document).ready(function () {
   	$.getScript('http://dppl.org/js/jquery.bcLists.js', function(){
-  		var list_id = $('.bc-list').attr('id').replace('bc-list-', '');
-  		$('head').append('<link rel="stylesheet" href="http://dppl.org/tools/bc/list-view/css/list.css" />');
-  		$('.bc-list').bcList({"list_id" : list_id});
+  		$('.bc-list').each(function(){
+			var list_id = $(this).attr('id').replace('bc-list-', '');
+			$(this).bcList({"list_id" : list_id});
+			$(this).find('.load').hide();
+		})
   	});
   });
-
 }
