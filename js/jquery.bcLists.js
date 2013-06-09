65a571724ca99a8a5381235c7aaca0f1ef72ff70
Brodie Austin <brodie.austin@gmail.com>
2013-06-08 23:17:08 -0500

    tweaks to bc jquery plugin for list viewer

--- js/jquery.bcLists.js
+++ js/jquery.bcLists.js
@@ -5,19 +5,23 @@
 	
 	var settings = $.extend({
 		'key' : '6vuvh66tx8evnz9b3r768gfh',
+		'library' : 'dppl',
+		'list_id' : '113135461',
 		'display_fields' : 'minimal',
+		'layout_class' : '',
+		'inline_styles' : '',
 		'show_title' : true,
+		'show_description' : false,
 		'show_thumbs' : true,
+		'show_annotations' : false,
 		'num_items' : 0,
 		'output' : 'html',
-		'list_id' : '113135461',
-		'library' : 'dppl'
 	}, options);
 	
 	var baseurl = (window.location.host == 'dppl.org'?'/tools/bc/list/':'http://dppl.org/tools/bc/list/');
 	var url  = baseurl + 'id/' + settings.list_id + '/library/' + settings.library + '/api_key/' + settings.key;
 	
-	var div = '';
+	var div, list_div, item_div;
 	var i = 0;
 	
 	return this.each(function() {
@@ -25,24 +29,33 @@
 		var $this = $(this);
 	
 		$.getJSON(url + '?jsoncallback=?', function(data){
+		    console.log(data);
+		    
+		    div = $('<div>', {
+		        'class' : 'bc-list',
+		        'id' : 'bc-list-' + settings.list_id
+		    });
+		    
 			if (settings.show_title == true){
-                switch (settings.output){
-                    case 'html':
-                        $this.before('<div class="list-title">' + data.list.name + '</div>');
-                        break;
-                    case 'text':
-                        $this.before('&lt;div class=&quot;list-title&quot;&gt;' + data.list.name + '&lt;/div&gt;');
-                        break;
-                }
-				
+                div.append('<div class="bc-list-title">' + data.list.name + '</div>');		
 			}
+			
+			if (settings.show_description == true){
+			    div.append('<div class="bc-list-description">' + data.list.description + '</div>');
+			}
+			
+			list_div = $('<div>', {
+			    'class' : 'bc-list-items ' + settings.layout_class,
+		        'style' : settings.inline_styles
+			});
+			
 			var items = data.list.list_items;
 			var len = ((settings.num_items == 0)?data.list.item_count:settings.num_items);
 			
 			$.each(items, function(i, item){
 				if (i < len){
-					div = $('<div>',{
-						'class': 'list-item',
+					item_div = $('<div>',{
+						'class': 'bc-list-item',
 					});
 			
 					item = data.list.list_items[i].title;
@@ -52,7 +65,8 @@
 						item_url = item['details_url'];
 				
 						a = $('<a>', {
-							'href': item_url
+							'href': item_url,
+							'class': (settings.show_thumbs == true?'bc-item-thumb':'')
 						});
 						
 						image_src = '';
@@ -94,33 +108,39 @@
 									break;
 							}
 							
-							//console.log(image_src)
 							
 							img = $('<img>',{
-								'class': 'bookjacket',
 								'src' : image_src
 							});
-					
+
 							a.append(img);
 						}
 						
-						a.append('<p>' + item['title'] + '</p>');
-						div.append(a);
+						a.append('<p class="bc-item-title">' + item['title'] + '</p>');
 						
-						switch (settings.output){
-							case 'html':
-								$this.append(div);
-								break;
-							case 'text':
-								//console.log(div[0].outerHTML);
-								$this.text($this.text() + div[0].outerHTML);
-								break;
+						if (settings.show_annotations == true){
+						    a.after('<p class="bc-item-annotation">' + data.list.list_items[i].annotation + '</p>');
 						}
-
+						
+						item_div.append(a);
+						list_div.append(item_div);
+						
 					}
 				}
 				
 			});
+			
+			div.append(list_div);
+			
+			switch (settings.output){
+				case 'html':
+					$this.html(div);
+					break;
+				case 'text':
+					//console.log(div[0].outerHTML);
+					$this.text(div[0].outerHTML);
+					break;
+			}
 			
 			
 			if(typeof callback == 'function'){
